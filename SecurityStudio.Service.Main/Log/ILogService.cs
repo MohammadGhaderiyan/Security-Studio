508508458d8ab04fcfd7088cac0aac7e21912178
Mohammad Ghaderiyan <mohammadghaderiyan@gmail.com>
2022-05-04 13:54:45 +0430

    Commit Some Code

---
+++ SecurityStudio.Service.Main/Log/ILogService.cs
@@ -0,0 +1,12 @@
+﻿using SecurityStudio.Base.Main.Mvvm;
+
+namespace SecurityStudio.Service.Main.Log
+{
+    public interface ILogService : IService
+    {
+        void Info(string info);
+        void Warning(string warning);
+        void Error(string error);
+        void Error(Exception exception);
+    }
+}
